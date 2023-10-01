56cdbee57903a9463e87b2d6af22c96773b32981
Lubomir80 <Kutsiyl@gmail.com>
2023-10-02 01:01:13 +0200

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import SettingsPage from "./pages/SettingsPage"
 import AccountPage from "./pages/AccountPage"
 import LoginPage from "./pages/LoginPage"
 import NotFoundOage from "./pages/NotFoundOage"
+import AppLayout from "./ui/AppLayout"
 
 
 function App() {
@@ -15,12 +16,14 @@ function App() {
          <GlobalStyles />
          <BrowserRouter>
             <Routes>
-               <Route index element={<Navigate replace to="dashbord" />} />
-               <Route path="dashbord" element={<DashbordPage />} />
-               <Route path="bookings" element={<BookingsPage />} />
-               <Route path="users" element={<UsersPage />} />
-               <Route path="settings" element={<SettingsPage />} />
-               <Route path="account" element={<AccountPage />} />
+               <Route element={<AppLayout />}>
+                  <Route index element={<Navigate replace to="dashbord" />} />
+                  <Route path="dashbord" element={<DashbordPage />} />
+                  <Route path="bookings" element={<BookingsPage />} />
+                  <Route path="users" element={<UsersPage />} />
+                  <Route path="settings" element={<SettingsPage />} />
+                  <Route path="account" element={<AccountPage />} />
+               </Route>
                <Route path="login" element={<LoginPage />} />
                <Route path="8" element={<NotFoundOage />} />
             </Routes>
