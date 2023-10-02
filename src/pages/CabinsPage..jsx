3ba72052790313bcc4f964bca0d09439db08494d
Lubomir80 <Kutsiyl@gmail.com>
2023-10-02 23:41:44 +0200

    supabase

--- src/pages/CabinsPage..jsx
+++ src/pages/CabinsPage..jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react"
+import { getCabins } from "../services/apiCabins"
 
 function CabinsPage() {
+   useEffect(() => {
+      getCabins().then(data => console.log(data))
+   }, [])
+
+
    return (
-      <div>CabinsPage</div>
+      <div>All Cabins
+         <img src="https://icdpmcxailuuqmjctfcq.supabase.co/storage/v1/object/public/cabin-img/cabin-001.jpg" alt="" />
+      </div>
    )
 }
 
