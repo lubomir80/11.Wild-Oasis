842dcb11b6f9a9dfee60335770fb7d6b2a5b7642
Lubomir80 <Kutsiyl@gmail.com>
2023-11-30 19:25:30 +0100

    code cleaning

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,50 +1,11 @@
+import { useState } from 'react';
 import toast from 'react-hot-toast';
-import styled from 'styled-components'
+import CreateCabinForm from './CreateCabinForm';
 import { formatCurrency } from '../../utils/helpers';
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from '../../services/apiCabins';
-import { useState } from 'react';
-import CreateCabinForm from './CreateCabinForm';
-
-
-const TableRow = styled.div`
-   display: grid;
-   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-   column-gap: 2.4rem;
-   align-items: center;
-   padding: 1.4rem 2.4rem;
-
-   &:not(:last-child) {
-      border-bottom: 1px solid var(--color-grey-100);
-   }
-   `
-
-const Img = styled.img`
-display: block;
-width: 6.4rem;
-aspect-ratio: 3 / 2;
-object-fit: cover;
-object-position: center;
-transform: scale(1.5) translateX(-7px);
-`;
-
-const Cabin = styled.div`
-  font-size: 1.6rem;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  font-family: 'Sono';
-`;
-
-const Price = styled.div`
-  font-family: 'Sono';
-  font-weight: 600;
-`;
-
-const Discount = styled.div`
-  font-family: 'Sono';
-  font-weight: 500;
-  color: var(--color-green-700);
-`;
+import { TableRow, Img, Cabin, Price, Discount } from "./CabinRowStyled"
+import Modal from '../../ui/Modal/Modal';
 
 
 function CabinRow({ cabin }) {
@@ -87,11 +48,17 @@ function CabinRow({ cabin }) {
             <Price>{formatCurrency(regularPrice)}</Price>
             <Discount>{formatCurrency(discount)}</Discount>
             <div>
-               <button onClick={() => setShowForm(show => !show)}>Edit</button>
-               <button disabled={isLoading} onClick={() => mutate(cabinId)}>Delete</button>
+               <Modal>
+                  <Modal.Open opens="edit">
+                     <button>Edit</button>
+                  </Modal.Open>
+                  <Modal.Window name="edit">
+                     <CreateCabinForm cabinToEdit={cabin} />
+                  </Modal.Window>
+                  <button disabled={isLoading} onClick={() => mutate(cabinId)}>Delete</button>
+               </Modal>
             </div>
          </TableRow >
-         {showForm && <CreateCabinForm cabinToEdit={cabin} />}
       </>
    )
 }
